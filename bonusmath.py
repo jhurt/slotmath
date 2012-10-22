dc809f58adc6d7717c49ad54da77dc99114a050e
Jason Hurt <jleehurt@gmail.com>
2012-10-22 15:44:00 -0700

    add script for simple choice bonus game

---
+++ bonusmath.py
@@ -0,0 +1,15 @@
+payouts_probabilities = []
+payouts_probabilities_file = open('bonus.csv', 'r')
+line = payouts_probabilities_file.readline()
+expected_value = 0.0
+while line:
+    tokens = line.split(',')
+    if len(tokens) != 2 or len(tokens[0]) < 1:
+        break
+    payout = float(tokens[0])
+    probability = float(tokens[1])
+    expected_value += payout * probability
+    line = payouts_probabilities_file.readline()
+payouts_probabilities_file.close()
+
+print "expected value: {0}".format(expected_value)
